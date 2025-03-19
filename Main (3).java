8533d6cbe76fcb10ed9cb1d9988d7644e50fa156
Cauan Amorim Gomes <162215277+CauanAmorimGomes@users.noreply.github.com>
2025-03-19 10:00:40 -0300

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class Main{
+    public static void main(String[] args){
+        double num1 = 10;
+        double num2 = 5;
+        
+        System.out.println("Soma: " + (num1 + num2));
+        System.out.println("Subtração: " + (num1 - num2));
+        System.out.println("Multiplacação : " + (num1 + num2));
+        System.out.println("Divisão: " + (num1/num2));
+    }
+}

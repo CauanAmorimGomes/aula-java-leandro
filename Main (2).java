13b5ce51fab5e67a7281026e986f66facebb0161
Cauan Amorim Gomes <162215277+CauanAmorimGomes@users.noreply.github.com>
2025-03-19 09:55:02 -0300

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,14 @@
+/******************************************************************************
+
+Classes e objetos
+Atributos e métodos
+
+*******************************************************************************/
+class Main {
+   public static void main(String[] args ){
+       double num1 = 10;
+       double num2 = 5;
+       
+       System.out.println("Soma: " + (num1 + num2));
+   }
+}

59228e3e45a3615f4be36cfb8992b6ad28d5741f
Cauan Amorim Gomes <162215277+CauanAmorimGomes@users.noreply.github.com>
2025-03-19 09:47:50 -0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    int numero = 10;
+	    if (numero > 0){
+	        System.out.println("Número positvo");
+	    } else {
+	        System.out.println("Número não positivo");
+	    }
+	}
+}

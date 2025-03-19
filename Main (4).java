23c43686a0da1275923e5e1b51cdda57d9a968b5
Cauan Amorim Gomes <162215277+CauanAmorimGomes@users.noreply.github.com>
2025-03-19 10:14:33 -0300

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class Main {
+    public static void main(String[] args){
+        int numero = 3;
+        if (numero%2 == 0){
+            System.out.println("O numero e par!");
+        } else
+    
+        System.out.println("O numero e impar!");
+    }
+}
